25ca66952cf2fe9f51ade5ed8bf681ccf8c3a1fe
kroften4 <134956188+kroften4@users.noreply.github.com>
2025-06-29 22:05:23 +0300

    fish nvim

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -gx vk_xwayland_wait_ready false
+set -gx MESA_VK_WSI_PRESENT_MODE immediate
+set -gx JAVA_HOME /opt/graalvm-jdk-21.0.7+8.1/
+
+alias todo "nvim /home/krft/Desktop/todo.txt"
+alias config "python ~/Utilities/config.py"
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME'
+
