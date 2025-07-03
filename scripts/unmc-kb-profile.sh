c01d0b8ef327178da9c346a3f96bf474a371a43a
kroften4 <134956188+kroften4@users.noreply.github.com>
2025-07-03 15:27:46 +0300

    tab-p keyd swap for mc

---
+++ scripts/unmc-kb-profile.sh
@@ -0,0 +1,7 @@
+# Set Hyprland layout (US/RU + Caps=Esc)
+hyprctl keyword input:kb_options "grp:alt_shift_toggle,caps:escape_shifted_capslock"
+
+# Disable Tab/P swap in keyd
+sudo sed -i '/^tab = p/d; /^p = tab/d' /etc/keyd/default.conf
+sudo systemctl restart keyd
+
