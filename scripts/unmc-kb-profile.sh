268828a0307be9be0e0e244a30eb6bba97c17f1b
kroften4 <134956188+kroften4@users.noreply.github.com>
2025-07-06 19:30:17 +0300

    zapret & caps-escape?

--- scripts/unmc-kb-profile.sh
+++ scripts/unmc-kb-profile.sh
@@ -1,5 +1,5 @@
 # Set Hyprland layout (US/RU + Caps=Esc)
-hyprctl keyword input:kb_options "grp:alt_shift_toggle,caps:escape_shifted_capslock"
+hyprctl keyword input:kb_options "grp:alt_shift_toggle,caps:escape"
 
 # Disable Tab/P swap in keyd
 sudo sed -i '/^tab = p/d; /^p = tab/d' /etc/keyd/default.conf
