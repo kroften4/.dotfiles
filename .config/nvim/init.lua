74ac1b4e6c55b64117a87d111da4d53a95449e57
kroften4 <134956188+kroften4@users.noreply.github.com>
2025-07-20 09:47:10 +0300

    fix tabs in insert mode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -841,6 +841,11 @@ require('lazy').setup({
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
+        ['<Tab>'] = {},
+        ['<S-Tab>'] = {},
+        ['<A-n>'] = { 'snippet_forward' },
+        ['<A-p>'] = { 'snippet_backward' },
+
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
